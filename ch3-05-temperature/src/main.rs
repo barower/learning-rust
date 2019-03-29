7246953c4e2194bf4cdb3d7200f1a638ced5654e
Bartlomiej Osinski <barower@gmail.com>
2019-03-29 23:58:45 +0100

    3-05-temperature: done

--- ch3-05-temperature/src/main.rs
+++ ch3-05-temperature/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_temp = String::new();
+
+    println!("Temperature in Celsius:");
+
+    io::stdin().read_line(&mut input_temp)
+        .expect("Failed to read line");
+
+    let input_temp = input_temp.trim().parse::<f64>()
+        .expect("Wrong number");
+
+    let output_temp = input_temp * 1.8 + 32.0;
+
+    println!("{} degrees Celsius is {} degrees Fahrenheit", input_temp, output_temp);
 }
