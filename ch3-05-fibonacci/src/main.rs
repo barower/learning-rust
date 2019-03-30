b1eb2317095edf5d9cf66574490b2213aa9f41e1
Bartlomiej Osinski <barower@gmail.com>
2019-03-30 10:48:03 +0100

    ch3-05-fibonacci: naive solution

--- ch3-05-fibonacci/src/main.rs
+++ ch3-05-fibonacci/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
+fn fibonacci(x: u64) -> u64 {
+    match x {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(x-1) + fibonacci(x-2)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Number:");
+
+    let mut x = String::new();
+    io::stdin().read_line(&mut x)
+        .expect("I/O error");
+    let x = x.trim().parse::<u64>()
+        .expect("Please give correct number");
+
+    let y = fibonacci(x);
+
+    println!("f({}) = {}", x, y);
 }
