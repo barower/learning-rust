2c2e18f09b48f41dee610ae99bbb010f6d6bf34f
Bartlomiej Osinski <barower@gmail.com>
2019-03-30 11:10:26 +0100

    ch3-05-fibonacci: iterative approach

--- ch3-05-fibonacci/src/main.rs
+++ ch3-05-fibonacci/src/main.rs
@@ -3,8 +3,17 @@ use std::io;
 fn fibonacci(x: u64) -> u64 {
     match x {
         0 => 0,
-        1 => 1,
-        _ => fibonacci(x-1) + fibonacci(x-2)
+        _ => {
+            let mut sum = 0;
+            let mut last = 0;
+            let mut current = 1;
+            for _i in 1..x {
+                sum = last + current;
+                last = current;
+                current = sum;
+            }
+            sum
+        }
     }
 }
 
